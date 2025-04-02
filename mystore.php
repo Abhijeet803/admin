7d542067764877fb80087365d9bf25cfb558c79d
Abhijeet Kumar <abhichy803@gmail.com>
2025-04-02 14:47:38 +0530

    testing

--- mystore.php
+++ mystore.php
@@ -16,10 +16,10 @@
 
 <?php
 
-session_start();
-if(!$_SESSION['admin']){
-  header("location:form/login.php");
-}
+// session_start();
+// if(!$_SESSION['admin']){
+//   header("location:form/login.php");
+// }
 
 ?>
 
